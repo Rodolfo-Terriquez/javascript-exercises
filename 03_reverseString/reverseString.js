5133d9417e12fbbd327e94ba689be0a0d564e909
Rodolfo <22480510+Rodolfo-Terriquez@users.noreply.github.com>
2023-01-02 21:55:53 -0600

    Completed exercise #3
    String reversal

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    let reversedArray = stringArray.reverse();
+    let reversedString = reversedArray.join('');
+    return reversedString;
 };
 
 // Do not edit below this line
