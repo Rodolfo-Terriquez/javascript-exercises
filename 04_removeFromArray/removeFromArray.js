ffdcbcb1a1be2e91c59c632fbcc57a4e669896f6
Rodolfo <22480510+Rodolfo-Terriquez@users.noreply.github.com>
2023-01-03 21:47:37 -0600

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...toRemove) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (toRemove.includes(array[i])) {
+        } else {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
