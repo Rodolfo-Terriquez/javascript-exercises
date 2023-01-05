8f2f2cbb0719c041a8e1dbe4a6bd60bf91ca4dca
Rodolfo <rodolfo.terriquez@gmail.com>
2023-01-04 18:49:35 -0600

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let countFrom;
+    let countTo;
+    //check for negative numbers
+    if (a < 0 || b < 0) { 
+        return 'ERROR';
+    //check for both being numbers
+    } else if (typeof(a) == 'number' && typeof(b) == 'number') {
+        if (a < b) {
+            countFrom = a;
+            countTo = b;
+        } else {
+            countFrom = b;
+            countTo = a;
+        }
+        let finalSum = 0;
+        for (let i = countFrom - 1; i < countTo + 1; i++) {
+            finalSum += i;
+        }
+        return finalSum;
+    //otherwise return ERROR
+    } else {
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
